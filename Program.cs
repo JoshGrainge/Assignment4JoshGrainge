afc8dfa6233e6be18cae45a5b843b6b87a8bdd32
JoshGrainge <josh.grainge@hotmail.com>
2021-11-13 19:53:17 -0500

    Assignment 4 Josh Grainge

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace A4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment these when you are ready to test a given task.
+            //Task1Tester.Test();
+            Task2Tester.Test();
+        }
+    }
+}
+
+
---
+++ Task2Interface.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace A4.Task2Interface
+{
+	interface ICarryPassengers
+	{
+		public abstract string CarryPassengers();
+    }
+	class Ship
+	{
+		public double Displacement { get; }
+
+		public Ship(double displacement)
+        {
+			Displacement = displacement;
+        }
+
+		public override string ToString() => $"A ship that displaces {Displacement} tons of water"; 
+	}
+
+	class CruiseShip : Ship, ICarryPassengers
+	{
+		public int NumPassengers;
+
+		public CruiseShip(double displacement, int numPassengers) : base (displacement)
+		{
+			NumPassengers = numPassengers;
+		}
+
+        public string CarryPassengers()
+        {
+			return $"{base.ToString()} and is carrying {NumPassengers} passengers on a Caribbean Cruise";
+		}
+
+        public override string ToString() => CarryPassengers();
+    }
+
+	class BigRig
+	{
+		public int NumTires;
+
+        public BigRig(int numTires)
+        {
+            NumTires = numTires;
+        }
+
+        public override string ToString() => $"A vehicle with {NumTires} tires"; 
+    }
+
+	class TourBus : BigRig, ICarryPassengers
+	{
+		public int NumPassengers;
+
+		public TourBus(int numTires, int numPassengers) : base(numTires)
+		{
+			NumPassengers = numPassengers;
+		}
+
+        public string CarryPassengers()
+        {
+			return $"{base.ToString()} and is carrying {NumPassengers} passengers on a cross country tour.";
+		}
+
+        public override string ToString() => CarryPassengers();
+    }
+}
